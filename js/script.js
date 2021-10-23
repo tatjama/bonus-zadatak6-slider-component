cf852eb4cd4802143c3d9f6110de92fccc15e7bd
Tanja <tanja120a@gmail.com>
2021-10-23 18:28:13 +0200

    Finished javascript

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
-/**Slider track progress */
+
 const slider = document.querySelector(".range__slider");
 const rangeLabel = document.querySelector(".range__label"); 
 const rangePrice = document.querySelector(".range__price");
+const toggleDiscountBtn = document.querySelector(".toggle__slider");
+toggleDiscountBtn.addEventListener("click", toggleDiscount);
 
-slider.oninput = function() {
+const updatePrice = (price) => {
+    rangePrice.textContent = (toggleDiscountBtn.classList.contains("active"))? 3*price/4 : price;
+}
+
+slider.oninput = function(){
   let trackValue = (this.value-this.min)/(this.max-this.min)*100;
   this.style.background = 
   'linear-gradient(to right, var(--slider-bar) 0%, var(--slider-bar) ' + trackValue 
   + '%, var(--empty-slider-bar) ' + trackValue + '%, var(--empty-slider-bar) 100%)'
 };
 
-slider.onchange = function(){
-    if(slider.value >= 10000 && slider.value < 50000){
+slider.onchange = () => {
+    if(slider.value < 50000){
         rangeLabel.textContent = "10K";
-        rangePrice.textContent = 8;
+        updatePrice(8);
     }else if(slider.value >= 50000 && slider.value < 100000){ 
         rangeLabel.textContent = "50K";
-        rangePrice.textContent = 12;
+        updatePrice(12);
     }else if(slider.value >= 100000 && slider.value < 500000){
         rangeLabel.textContent = "100K";
-        rangePrice.textContent = 16;
+        updatePrice(16);
     }else if(slider.value >= 500000 && slider.value < 1000000){
-        rangeLabel.textContent = "500K";        
-        rangePrice.textContent = 24;
+        rangeLabel.textContent = "500K";
+        updatePrice(24);
     }else{
-        rangeLabel.textContent = "M";
-        rangePrice.textContent = 36;
+        rangeLabel.textContent = "1M";
+        updatePrice(36);
     } 
 }
+
+function toggleDiscount(){    
+    rangePrice.textContent = (this.classList.toggle("active"))? 
+    3 * rangePrice.textContent*1 / 4 : 4 * rangePrice.textContent*1 / 3;
+}
+
+
